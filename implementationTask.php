3d33fea8ff547efe9a8e5d52bb3a3302dc6d395f
Asekur <lina.rusinovich@mail.ru>
2020-04-05 00:49:58 +0300

    Added constants, fixed codestyle

--- implementationTask.php
+++ implementationTask.php
@@ -1,9 +1,12 @@
 <?php
+const CHAR_0 = 48;
+const CHAR_9 = 57;
+
 $totalSum=0;
 if (isset($_POST['submit']) && isset($_POST['number'])){
-    $number=$_POST['number'];
-    for ($i = 0; $i < strlen ( $number ); $i++) {
-        if (ord ( $number[$i] ) >= 48 && ord ( $number[$i] ) <= 57) {
+    $number = $_POST['number'];
+    for ($i = 0; $i < strlen ($number); $i++) {
+        if (ord ( $number[$i] ) >= CHAR_0 && ord ( $number[$i] ) <= CHAR_9) {
             $totalSum += (int)$number[$i];
         }
     }
