87d56eecbaf98951b053448ea7ba6aecb1c9b111
Asekur <lina.rusinovich@mail.ru>
2020-04-13 20:58:06 +0300

    Changed colored menu

--- menuOutput.php
+++ menuOutput.php
@@ -1,33 +1,33 @@
-<form class="header-addTask-form" method="get" action="index.php">
-    <?php if (isset($_GET['coloredMenu']) && 'HOME' == $_GET['coloredMenu']) { ?>
+<form class="header-addTask-form" method="post" action="index.php">
+    <?php if ($_SERVER['PHP_SELF'] == '/index.php') { ?>
     <input type="submit" name="coloredMenu" value="HOME" id="phpBG">
     <?php } else { ?>
     <input type="submit" name="coloredMenu" value="HOME">
     <?php } ?>
 </form>
-<form class="header-addTask-form" method="get" action="about.php">
-    <?php if (isset($_GET['coloredMenu']) && 'ABOUT US' == $_GET['coloredMenu']) { ?>
+<form class="header-addTask-form" method="post" action="about.php">
+    <?php if ($_SERVER['PHP_SELF'] == '/about.php') { ?>
     <input type="submit" name="coloredMenu" value="ABOUT US" id="phpBG">
     <?php } else { ?>
     <input type="submit" name="coloredMenu" value="ABOUT US">
     <?php } ?>
 </form>
-<form class="header-addTask-form" method="get" action="news.php">
-    <?php if (isset($_GET['coloredMenu']) && 'NEWS' == $_GET['coloredMenu']) { ?>
+<form class="header-addTask-form" method="post" action="news.php">
+    <?php if ($_SERVER['PHP_SELF'] == '/news.php') { ?>
     <input type="submit" name="coloredMenu" value="NEWS" id="phpBG">
     <?php } else { ?>
     <input type="submit" name="coloredMenu" value="NEWS">
     <?php } ?>
 </form>
-<form class="header-addTask-form" method="get" action="competitons.php">
-    <?php if (isset($_GET['coloredMenu']) && 'COMPETITIONS' == $_GET['coloredMenu']) { ?>
+<form class="header-addTask-form" method="post" action="competitons.php">
+    <?php if ($_SERVER['PHP_SELF'] == '/competitons.php') { ?>
     <input type="submit" name="coloredMenu" value="COMPETITIONS" id="phpBG">
     <?php } else { ?>
     <input type="submit" name="coloredMenu" value="COMPETITIONS">
     <?php } ?>
 </form>
-<form class="header-addTask-form" method="get" action="contacts.php">
-    <?php if (isset($_GET['coloredMenu']) && 'CONTACTS' == $_GET['coloredMenu']) { ?>
+<form class="header-addTask-form" method="post" action="contacts.php">
+    <?php if ($_SERVER['PHP_SELF'] == '/contacts.php') { ?>
     <input type="submit" name="coloredMenu" value="CONTACTS" id="phpBG">
     <?php } else { ?>
     <input type="submit" name="coloredMenu" value="CONTACTS">
