e3dfca1a781e1ac47bb4179249e1f08e6768c9f5
Asekur <lina.rusinovich@mail.ru>
2020-04-25 18:24:47 +0300

    Added task based on regular expressions

--- implementationTask.php
+++ implementationTask.php
@@ -2,8 +2,9 @@
 const CHAR_0 = 48;
 const CHAR_9 = 57;
 
-$totalSum=0;
-if (isset($_POST['submit']) && isset($_POST['number'])) {
+$totalSum = 0;
+if (isset($_POST['submit']) && isset($_POST['number'])) 
+{
     $number = $_POST['number'];
     for ($i = 0; $i < strlen ($number); $i++) {
         if (ord ( $number[$i] ) >= CHAR_0 && ord ( $number[$i] ) <= CHAR_9) {
